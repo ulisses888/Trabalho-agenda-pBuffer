339cffd7e0f79a21041707c1c2c633484933b717
ulisses888 <159669337+ulisses888@users.noreply.github.com>
2025-01-29 19:37:29 -0300

    Update main.c

--- main.c
+++ main.c
@@ -81,55 +81,56 @@ void menu() {
 
 
 	}
-
+	
 }
 void adicionarPessoa() {
 
 	int* numPessoas = (int*)pBuffer;
-	int* idade = (int*)pBuffer + 2;
-	char* nome = (char*)pBuffer + 3 * sizeof(int);
-	char* email = (char*)pBuffer + 3 * sizeof(int) + 100 * sizeof(char);
+	int* idadeTemp = (int*)pBuffer + 2;
+	char* nomeTemp = (char*)pBuffer + 3 * sizeof(int);
+	char* emailTemp = (char*)pBuffer + 3 * sizeof(int) + 100 * sizeof(char);
 	void* escrever;
 
 
 
 
 	printf("\nDigite o nome completo da pessoa: (max 100 caracteres) ");
-	fgets(nome, 100, stdin);
-	nome[strcspn(nome, "\n")] = 0;
+	fgets(nomeTemp, 100, stdin);
+	nomeTemp[strcspn(nomeTemp, "\n")] = 0;
 
 	printf("\nDigite a idade da pessoa: ");
-	scanf_s("%d", idade);
+	scanf_s("%d", idadeTemp);
 	while (getchar() != '\n');
 
 	printf("\nDigite o email da pessoa: ");
-	fgets(email, 100, stdin);
+	fgets(emailTemp, 100, stdin);
 
-	printf("Nome %s\n", nome);
-	printf("Email %s\n", email);
-	printf("Idade %d\n", *idade);
+	printf("Nome %s\n", nomeTemp);
+	printf("Email %s\n", emailTemp);
+	printf("Idade %d\n", *idadeTemp);
 
-	printf("\n num pessoas %d\n", *numPessoas);
-
-
-
-
-	pBuffer = realloc(pBuffer, (3 * sizeof(int) + 100 * sizeof(char) + 100 * sizeof(char)) + (*numPessoas * 1 * sizeof(int) + 100 * sizeof(char) + 100 * sizeof(char)));
+	pBuffer = realloc(pBuffer, 3 * sizeof(int) + 200 * sizeof(char) + ((*numPessoas +1 ) * (sizeof(int) + 200 * sizeof(char))));
 	if (!pBuffer) {
 		printf("Erro ao realocar memoria\n");
+		free(pBuffer);
 		exit(1);
 	}
 
-	(*(int*)pBuffer)++;
 	numPessoas = (int*)pBuffer;
-	printf("num pos incremento %d", *numPessoas);
+	idadeTemp = (int*)pBuffer + 2;
+	nomeTemp = (char*)pBuffer + 3 * sizeof(int);
+	emailTemp = (char*)pBuffer + 3 * sizeof(int) + 100 * sizeof(char);
+	
+	(*numPessoas)++;
 
-	(int*)escrever = (int*)pBuffer + 2 + (*numPessoas * (sizeof(int) + 200 * sizeof(char)));
-	memcpy(escrever, idade, sizeof(int));
-	(char*)escrever = (char*)pBuffer + 3 * sizeof(int) + *numPessoas * (sizeof(int) + 200 * sizeof(char));
-	memcpy(escrever, nome, 100 * sizeof(char));
-	(char)escrever = (char*)pBuffer + 3 * sizeof(int) + *numPessoas * (sizeof(int) + 200 * sizeof(char)) + 100 * sizeof(char);
-	memcpy(escrever, email, 100 * sizeof(char));
+	escrever = (char*)pBuffer + 2 * sizeof(int) + (*numPessoas * (sizeof(int) + 200 * sizeof(char)));
+	memcpy(escrever, idadeTemp, sizeof(int));
+	escrever = (char*)pBuffer + 2 * sizeof(int) + (*numPessoas * (sizeof(int) + 200 * sizeof(char)));
+	memcpy(escrever, nomeTemp, 100 * sizeof(char));
+	escrever = (char*)pBuffer + 2 * sizeof(int) + *numPessoas * (sizeof(int) + 200 * sizeof(char)) + 100 * sizeof(char);
+	memcpy(escrever, emailTemp, 100 * sizeof(char));
+
+
 
 
 
@@ -144,8 +145,10 @@ void listarPessoas() {
 	int* numPessoas = (int*)pBuffer;
 	int* contador = (int*)pBuffer + 1;
 	int* idade;
+	char* idadeEndereco;
 	char* nome;
 	char* email;
+
 	printf("valor pBuffer %d\n", (*(int*)pBuffer));
 	printf("num pessoas %d\n",*numPessoas);
 	if (*numPessoas == 0) {
@@ -157,12 +160,13 @@ void listarPessoas() {
 		printf("----------------");
 		printf("\n\nLISTA DE PESSOAS:\n\n");
 
-		for (*contador = 0; *contador < *numPessoas; contador++) {
+		for (*contador = 0; *contador < *numPessoas; (*contador)++) {
 			nome = (char*)pBuffer + 3 * sizeof(int) + *contador * (sizeof(int) + 200 * sizeof(char));
-			idade = (int*)pBuffer + 2 + (*contador * (sizeof(int) + 200 * sizeof(char)));
+			idadeEndereco = (char*)pBuffer + 2 * sizeof(int) + (*contador * (sizeof(int) + 200 * sizeof(char)));
+			idade = (int*)idadeEndereco;
 			email = (char*)pBuffer + 3 * sizeof(int) + *contador * (sizeof(int) + 200 * sizeof(char)) + 100 * sizeof(char);
 
-			printf("[%d] Nome = %s | Idade = %d | Email = %s\n", contador + 1, nome, *idade, email);
+			printf("[%d] Nome = %s | Idade = %d | Email = %s\n", *contador + 1, nome, *idade, email);
 
 		}
 		printf("----------------");
