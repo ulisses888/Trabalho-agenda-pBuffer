7378e9f66b96ff3376f93bf91e99e20b8eed0ce8
ulisses888 <159669337+ulisses888@users.noreply.github.com>
2025-01-29 16:02:32 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <String.h>
 
-void* pBuffer;
+void* pBuffer = NULL;
 
 void adicionarPessoa();
 void removerPessoa();
@@ -13,12 +13,13 @@ void menu();
 
 void main() {
 
-	pBuffer = malloc(sizeof(int));
+	// 1 Int para armazenar n pessoas 2 int para menu 3 int idade temp 100 char nome e 100 char email temporario tbm
+	pBuffer = malloc(3 * sizeof(int) + 100 * sizeof(char) + 100 * sizeof(char));
 	if (!pBuffer) {
 		printf("Erro ao alocar memoria\n");
 		exit(1);
 	}
-	// Aqui sera o contador de pessoas
+	// Contador de Pessoas
 	*((int*)pBuffer) = 0;
 	for (;;) {
 		menu();
@@ -31,14 +32,10 @@ void main() {
 
 void menu() {
 
-    int* menu;
+	int* menu;
 
-    menu = malloc(sizeof(int));
-    if (!menu) {
-        printf("Erro ao alocar memoria (menu)");
-        exit(1);
-    }
-	
+	menu = ((int*)pBuffer + 1);
+
 	printf("\nEscolha uma opcao:\n");
 	printf("1 - Adicionar pessoa\n");
 	printf("2 - Remover Pessoa\n");
@@ -47,10 +44,9 @@ void menu() {
 	printf("5 - Sair\n\n");
 
 
-	if (scanf_s("%d", *menu) != 1) {
+	if (scanf_s("%d", menu) != 1) {
 		printf("Erro ao ler a opcao\n");
 		free(pBuffer);
-        	free(menu);
 		exit(1);
 	}
 
@@ -76,32 +72,27 @@ void menu() {
 	case 5:
 		printf("\n\nFechando a Agenda\n\n");
 		free(pBuffer);
-        	free(menu);
 		exit(0);
 
 	default:
 		printf("Opcao Invalida");
 		break;
 
-    
+		
 
 	}
-	
-	free(menu);
+
 }
 void adicionarPessoa() {
-	
-	int* numPessoas = (int*)pBuffer;
-	int* idade = malloc(sizeof(int));
-	char* nome = malloc(sizeof(char) * 100);
-	char* email = malloc(sizeof(char) * 320);
-	
 
-	if (!numPessoas || !idade || !nome || !email) {
-		printf("Erro ao alocar memoria\n");
-		exit(1);
-	}
-	
+	int* numPessoas = (int*)pBuffer;
+	int* idade = (int*)pBuffer + 2;
+	char* nome = (char*)pBuffer + 3 * sizeof(int);
+	char* email = (char*)pBuffer + 3 * sizeof(int) + 100 * sizeof(char);
+
+
+
+
 	printf("\nDigite o nome completo da pessoa: (max 100 caracteres) ");
 	fgets(nome, 100, stdin);
 	nome[strcspn(nome, "\n")] = 0;
@@ -111,30 +102,26 @@ void adicionarPessoa() {
 	while (getchar() != '\n');
 
 	printf("\nDigite o email da pessoa: ");
-	fgets(email, 320, stdin);
-	
-	printf("Nome %s\n",nome);
-	printf("Email %s\n",email);
+	fgets(email, 100, stdin);
+
+	printf("Nome %s\n", nome);
+	printf("Email %s\n", email);
 	printf("Idade %d\n", *idade);
 
-
 	printf("\n num pessoas %d\n", *numPessoas);
-	*numPessoas++;
-	*((int*)pBuffer) = *numPessoas;
+	(*(int*)pBuffer)++;
 	
-    printf("num pos incremento %d", *((int*)pBuffer));
-	
-	pBuffer = realloc(pBuffer, 2*sizeof(int) + *numPessoas * (sizeof(int) + 100 * sizeof(char) + 320 * sizeof(char)));
+	printf("num pos incremento %d", *((int*)pBuffer));
+
+	pBuffer = realloc(pBuffer, (3 * sizeof(int) + 100 * sizeof(char) + 100 * sizeof(char)) + (*numPessoas * 1 * sizeof(int) + 100 * sizeof(char) + 100 * sizeof(char)));
 	if (!pBuffer) {
 		printf("Erro ao realocar memoria\n");
 		exit(1);
 	}
 
+	
 
 
-	free(nome);
-	free(email);
-	free(idade);
 }
 void removerPessoa() {
 	printf("remover pessoa");
